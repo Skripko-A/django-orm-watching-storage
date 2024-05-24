e72e2048484081096a92a89abf16d6d5f2cbb36e
skripko-a <skripkoae@gmail.com>
2024-05-24 09:27:28 +0300

    delete needless get_storage_visits function

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,20 +1,20 @@
-from datacenter.models import Passcard, get_duration, get_storage_visits_personal, is_visit_long, format_duration
+from datacenter.models import Passcard, Visit, get_duration, is_visit_long, format_duration
 from django.shortcuts import render, get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    passcard = get_object_or_404(Passcard, passcode=passcode)
-    personal_visits = get_storage_visits_personal(passcode)
+    person = get_object_or_404(Passcard, passcode=passcode)
+    person_storage_visits = Visit.objects.filter(passcard=person)
     this_passcard_visits = [
         {
             'entered_at': visit.entered_at,
             'duration': format_duration(get_duration(visit)),
             'is_strange': is_visit_long(visit, 60)
         }
-        for visit in personal_visits
+        for visit in person_storage_visits
     ]
     context = {
-        'passcard': passcard,
+        'passcard': person,
         'this_passcard_visits': this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
